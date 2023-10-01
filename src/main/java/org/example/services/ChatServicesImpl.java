34c2fea9032e303ede3c824e903518ac097a94ef
Stepan6582 <prokofev02@bk.ru>
2023-10-01 23:39:40 +0300

    Message implementation changed again

--- src/main/java/org/example/services/ChatServicesImpl.java
+++ src/main/java/org/example/services/ChatServicesImpl.java
@@ -3,7 +3,6 @@ package org.example.services;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.example.domain.Message;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -13,12 +12,12 @@ public class ChatServicesImpl implements ChatServices {
     private final ServicesOfServerImpl services;
     private final ClientServicesImpl client;
     private final Socket socket;
-    private final Message message;
 
     @Override
     @SneakyThrows
     public void chat() {
         BufferedReader readerFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        Message message = new Message();
 
         client.notifyMe("!chat!");
         String messageFromClient;
--- src/main/java/org/example/services/ClientRunnable.java
+++ src/main/java/org/example/services/ClientRunnable.java
@@ -3,7 +3,6 @@ package org.example.services;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.example.dao.UserDao;
-import org.example.domain.Message;
 
 import java.net.Socket;
 
@@ -17,13 +16,12 @@ public class ClientRunnable implements Runnable {
     @Override
     public void run() {
         ClientServicesImpl client = new ClientServicesImpl(userDao, socket);
-        Message message = new Message();
 
         while (true) {
             MenuServices menuServices = new MenuServicesImpl(services, client, socket);
             menuServices.menu();
 
-            ChatServices chatServices = new ChatServicesImpl(services, client, socket, message);
+            ChatServices chatServices = new ChatServicesImpl(services, client, socket);
             chatServices.chat();
         }
     }
